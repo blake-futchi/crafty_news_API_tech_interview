336af38995c6da49be853804b1f26ca0a0a4d7e0
Blake <blake@futchi.com>
2020-04-22 16:41:06 +0200

    added create function in controller with json response, removed correlation to article id

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,4 +1,6 @@
 class Api::CommentsController < ApplicationController
   def create
+    comment = Comment.create()
+    render json: { message: 'Your comment has been created' }
   end
 end
--- spec/requests/api/comments/user_can_comment.spec.rb
+++ spec/requests/api/comments/user_can_comment.spec.rb
@@ -1,16 +1,25 @@
 RSpec.describe 'POST /api/comment', type: :request do
-  let!(:article) { create(:article) }
-  
+  describe 'valid credentials input' do
+    let(:user) { create(:user) }
+    let(:user_credentials) { user.create_new_auth_token }
+  end
   describe 'successfully creates comment' do
+
+    let!(:article) { create(:article) }
     before do 
-      post '/api/comments'
+      post '/api/comments',
+      params: {
+        comment: {
+          body: 'you created a comment'
+        }
+      }
     end
 
     it 'returns a 200 response' do
       expect(response).to have_http_status 200
     end
 
-    it 'returns three articles' do
+    it 'returns a successful response message' do
       expect(response_json['message']).to eq 'Your comment has been created'
     end
   end
